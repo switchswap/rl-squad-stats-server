8445f42a27e68ef40630194322eb83a700aafb43
Swap <sp70215@gmail.com>
2023-11-30 01:25:18 -0800

    Fix Dockerfile and add logs

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM python:3.9
 WORKDIR /code
 COPY ./requirements.txt /code/requirements.txt
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
-COPY ./* /code/
+COPY ./ /code/
 RUN chmod a+x /code/start.sh
-CMD ["./code/start.sh"]
+CMD ["/code/start.sh"]
--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo "[RL-Server] Starting replay scraper"
 exec python3 replay_scraper.py &
-exec uvicorn app.main:app --proxy-headers --host 0.0.0.0 --port 80
+echo "[RL-Server] Starting server"
+exec uvicorn main:app --proxy-headers --host 0.0.0.0 --port 80
